929bd92a23b06bfb70d4a38d32a5fd3105b7d1b6
newtpowers <cjp245@cornell.edu>
2023-09-16 20:17:28 -0400

    feat: make helper functions to calculate motion

--- src/Projectile.cpp
+++ src/Projectile.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 /**The initial height of the projectile, which is always 0 meters above the ground.*/
@@ -6,7 +7,7 @@ using namespace std;
 /**The initial horizontal position of the projectile, which is always 0 meters from the point of launch.*/
 #define initialHorizontalPosition 0.0
 /**The gravitational acceleration due to Earth, which is always 9.81 m/s^2.*/
-#define g -9.81
+#define g 9.81
 
 /**
  * The projectile launched in the projectile motion simulator.
@@ -16,8 +17,6 @@ class Projectile {
     double initialSpeed;
     /**The angle above the ground the projectile is pointed. Requires: 'angle' is from 0.0 to 90.0 degrees*/
     double angle;
-    /**The mass of the projectile in kg. Requires: 'mass' is non-negative.*/
-    double mass;
     /**The horizontal distance the projectile traveled in m.*/
     double distance;
     /**The time the projectile is off the ground in s.*/
@@ -32,19 +31,31 @@ class Projectile {
         void assertInv() {
             assert(initialSpeed > 0);
             assert(angle >= 0 && angle <= 90);
-            assert(mass >= 0);
+        }
+
+        /**
+         * Return the horizontal component of the 'initialSpeed'.
+        */
+        double x() {
+            return initialSpeed * cos(angle);
+        }
+
+        /**
+         * Return the vertical component of the 'initialSpeed'.
+        */
+        double y() {
+            return initialSpeed * sin(angle);
         }
 
     public:
         /**
-         * Create a new Projectile with initial speed 'initSpeed', angle 'ang', and mass 'mas'. 
-         * Requires: initSpeed, ang, and mas are non-empty.
+         * Create a new Projectile with initial speed 'initSpeed' and angle 'ang'. 
+         * Require: initSpeed, ang are non-empty.
         */
-        Projectile(double initSpeed, double ang, double mas) {
+        Projectile(double initSpeed, double ang) {
             //assign global variables given inputs
             initialSpeed = initSpeed;
             angle = ang;
-            mass = mas;
             assertInv();
         }
 
@@ -52,38 +63,24 @@ class Projectile {
         /**
          * Return the horizontal distance the projectile traveled before hitting the ground.
         */
-        double distance() {
+        double distanceNum() {
+            //can't have method and variables have same name
             return distance;
         }
 
         /**
          * Return the time taken during projectile motion.
         */
-        double time() {
+        double timeNum() {
             return time;
         }
 
         /**
          * Return the highest point of the projectile.
         */
-        double apex() {
+        double apexNum() {
             return apex;
         }
-
-        /**
-         * Calculate the projectile properties.
-        */
-        int main() {
-            //horizontal motion
-            //v0x = vx
-            //use y = y0 + v0y(t) - (1/2)g(t^2) to find t where t is not 0
-            //then, use x = x0 + vx(t) to find x, the total horizontal distance
-            //vy = 0 at apex
-            //now, use vy^2 = v0y^2 - 2g(y-y0) to find the apex, y
-            return 0;
-        }
-
-        //Create three helper methhods for the horizontal and vertical components of the speed and the total time, distance, and apex
 
 };
 
